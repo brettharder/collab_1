31f5fe3730cb04ac4e22ae7fee7f3b0b2381985c
sharder14 <31424252+sharder14@users.noreply.github.com>
2018-12-01 14:23:38 -0500

    Phonebook finished AJAX

--- phonebook/views.py
+++ phonebook/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views import generic
 
 from .models import Contact 
 
+import json
+
 # Create your views here.
 def phonebook(request):
-    print(request.POST)
+    if(request.POST):
+        if(request.POST.get("newContactName1")):
+            name=request.POST.get("newContactName1")
+            if(request.POST.get("newContactNumber1")):
+                number=request.POST.get("newContactNumber1")
+                contact=Contact(name=name,number=number)
+                contact.save()
+        if(request.is_ajax):
+            print("its ajax")
+            if(request.POST.get('newContactName2')):
+                name=request.POST.get("newContactName2")
+                if(request.POST.get("newContactNumber2")):
+                    number=request.POST.get("newContactNumber2")
+                    contact=Contact(name=name,number=number)
+                    contact.save()
+    if(request.GET.get('deleteContact')):
+        tid=request.GET.get('deleteContact')
+        contact=Contact.objects.get(id=tid)
+        contact.delete()
     contacts= Contact.objects.all()
     content={'contacts':contacts}
     return render(request, "phonebook/phonebook.html",content)
